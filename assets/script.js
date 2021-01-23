9ab90e9abc3996d1bf6057e4168f46a679f0230a
Sooner Writer <larrygjenkins@gmail.com>
2021-01-22 22:07:01 -0800

    added timer countdown and included with Start button event listener

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
 // var startBtn = document.querySelector("#startBtn");
 
-
-
-
+var timer = 30;
 
 $(".startBtn").on("click", function(){
     console.log("The button works");
+    var countdown = setInterval(startCountdown, 1000);
+
+    function startCountdown() {
+       $("#counter").text(timer);
+       timer--;
+      }
+
 })
