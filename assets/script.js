50adb44f7128a5b7d3b8adbdb13162b34c8611db
Sooner Writer <larrygjenkins@gmail.com>
2021-01-23 07:25:22 -0800

    added question bank variable with an array of question/answer objects

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
 // var startBtn = document.querySelector("#startBtn");
 
 var timer = 30;
+var questionBank = [
+    {
+    Question:  "Variables are made up of:",
+    Answer: ["a vowel and consonant", "an object and a location", "a name and a value"],
+    correctAnswer: "a name and a value",
+    }, 
+
+    {
+    Question:  "Which method displays data in the browser's console?",
+    Answer: ["alert", "console.log", "window.print"],
+    correctAnswer: "console.log",
+    }, 
+
+    {
+    Question:  "What is the index of grapes in the following array: [oranges, grapes, apples]",
+    Answer: ["1", "2", "3"],
+    correctAnswer: "1",
+        } 
+
+]
 
 function addContent() {
     $("#quizContent").text("My Quiz");
